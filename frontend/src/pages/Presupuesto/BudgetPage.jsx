01a52b88cebe45cc93c89cbd04e461836cb93380
JorgeGcode <jorge.antonio.guzman@mi.unc.edu.ar>
2026-01-17 17:52:17 -0300

    pagina de presupuesto

---
+++ frontend/src/pages/Presupuesto/BudgetPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BudgetPage = () => {
+  return (
+    <div style={{ padding: '50px', textAlign: 'center' }}>
+      <h1>Solicitud de Presupuesto 📝</h1>
+      <p>Acá es donde vamos a construir el formulario para Botmania.</p>
+    </div>
+  );
+};
+
+export default BudgetPage;
